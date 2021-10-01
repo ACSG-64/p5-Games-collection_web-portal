11cb5185f4f4bd3054c8db27a8479de909fb672c
Andrés Sarmiento <acamilo707@gmail.com>
2021-10-01 16:36:53 -0500

    Added the method to delete users and the addUser method now returns only the user id.

--- models/user/userModel.js
+++ models/user/userModel.js
@@ -1,13 +1,19 @@
 const db = require('../../database/db');
 
-function addUser(dataObj) {
+async function addUser(dataObj) {
     const {userName, name, lastname, password} = dataObj;
-    return db.query('INSERT INTO users (user_name, pswd, names, lastnames, is_active) VALUES ($1, $2, $3, $4, $5) RETURNING user_id',
+    const result = await db.query('INSERT INTO users (user_name, pswd, names, lastnames, is_active) VALUES ($1, $2, $3, $4, $5) RETURNING user_id',
         [userName, password, name, lastname, false])
+    return result.rows[0].user_id;
 }
 
-function addActivationCode(userId, confCode) {
-    return db.query('INSERT INTO activation_codes (user_id, code) VALUES ($1, $2)', [userId, confCode])
+async function retrieveUser(userName) {
+    const results = await db.query('SELECT user_id, user_name, pswd, is_active FROM users WHERE user_name=$1', [userName]);
+    return results.rows[0]
+}
+
+async function removeUser(userId) {
+    await db.query('DELETE FROM users WHERE user_id=$1', [userId]);
 }
 
 async function changeAccountStatus(confCode, newState = true) {
@@ -19,9 +25,8 @@ async function removeActivationCode(confCode) {
     await db.query('DELETE FROM activation_codes WHERE code=$1', [confCode]);
 }
 
-async function retrieveUser(userName) {
-    const results = await db.query('SELECT user_id, user_name, pswd, is_active FROM users WHERE user_name=$1', [userName]);
-    return results.rows[0]
+function addActivationCode(userId, confCode) {
+    return db.query('INSERT INTO activation_codes (user_id, code) VALUES ($1, $2)', [userId, confCode])
 }
 
 function updateUser(dataObj) {
@@ -29,6 +34,7 @@ function updateUser(dataObj) {
 
 module.exports = {
     addUser,
+    removeUser,
     addActivationCode,
     activateAccount: changeAccountStatus,
     removeActivationCode,
